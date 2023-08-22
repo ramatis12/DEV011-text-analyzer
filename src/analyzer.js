4002eafea47c50f03576dec489d6ec41bfde54ce
Sara Ramirez <sara_ramatis@hotmail.com>
2023-08-22 17:49:46 -0600

    martes

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,6 +1,7 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    console.log("The empty string has a length of " + cadena.length);
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+        const cadena = document.querySelector("suer-input");
+
+
+        // leer todos los <li>
+        // Revisar su el selector es el correcto
+        const palabra = document.querySelector(".palabra");
+        const caracter = document.querySelector(".caracter");
+        const espacio = document.querySelector(".espacio");
+        const numero = document.querySelector(".numero");
+        const suma = document.querySelector(".suma");
+        const media = document.querySelector(".media");
+
+        cadena.addEventListener("keyup", analyzer);
+
+        // text.addEventListener("click", text, false);
+        console.log(text);
