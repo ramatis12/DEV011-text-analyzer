b4e6113ec3cfe4eb594be685ac8468896fd13fbc
Sara Ramirez <sara_ramatis@hotmail.com>
2023-08-30 12:16:01 -0600

    miercoles

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,42 +1,46 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    let pal= text.split(" ");
+    const pal = text.trim().split(" ")
+
     return pal.length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-    let item= 0;
-       for (let i = 0; i <text.length; i++) {
-              item++; 
-        }
-     return item; 
+    let item = 0;
+    for (let i = 0; i < text.length; i++) {
+      item++;
+    }
+    return item;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    
-    //let esp = text.trim( );
-    //return esp.length;
-    
-    let sin = text;
-    return sin.replace( /\s/g, '' ).length
+    const sin = text.replace(/\s/g, '').replace(/[.,#!"$%&;:{}=\-_~()]+/g, '');
+    return sin.length;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    return text
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    let nums= 0;
-    for (let i = 0; i <text.length; i++) {
-      if (i != isNaN(text.length)) {
-        nums++; 
+    let numse = 0;
+    const sin = text.replace(/\s/g, ' ').trim().split(" ");
+    //console.log('=> ', sin);
+    for(let i = 0; i < sin.length; i++) {
+      // let nums = sin[i];
+      if (!isNaN(sin[i])) {
+        numse++;
       }
-      
-}
-return nums; 
+    }
+    return numse;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    //let n = 0;
+    //const s = text.replace(/\s/g, ' ').trim().split(" ");
+    console.log(text.length);
+
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 //decalracion de variables
-        let text = document.querySelector('[name="suer-input"]');
-        let palabra = document.querySelector(".palabra");
-        let caracter = document.querySelector(".caracter");
-        let espacio = document.querySelector(".espacio");
-        let numero = document.querySelector(".numero");
-        let suma = document.querySelector(".suma");
-        let  media = document.querySelector(".media");
+const text = document.querySelector('[name="suer-input"]');
+const palabra = document.querySelector(".palabra");
+const caracter = document.querySelector(".caracter");
+const espacio = document.querySelector(".espacio");
+const numero = document.querySelector(".numero");
+const suma = document.querySelector(".suma");
+const media = document.querySelector(".media");
+console.log(suma, media);
 
 //llamar las funciones del contador
-       text.addEventListener("keyup", (primera) =>{
-        palabra.innerHTML = analyzer.getWordCount(text.value);
-        caracter.innerHTML = analyzer.getCharacterCount(text.value); 
-        espacio.innerHTML = analyzer.getCharacterCountExcludingSpaces(text.value);
-        console.log(analyzer.getNumberCount(text.value));
-       });
+text.addEventListener("keyup", () => {
+  palabra.innerHTML = analyzer.getWordCount(text.value);
+  caracter.innerHTML = analyzer.getCharacterCount(text.value);
+  espacio.innerHTML = analyzer.getCharacterCountExcludingSpaces(text.value);
+  numero.innerHTML = analyzer.getNumberCount(text.value);
+  //suma.innerHTML = analyzer.getNumberSum(text.value);
+});
 
 //funcion para limpiar el contenido de textarea
-       let limpiar = document.getElementById("reset-button");
-       limpiar.addEventListener("click", () =>{
-        text.value = "";
-       });
+const limpiar = document.getElementById("reset-button");
+limpiar.addEventListener("click", () => {
+  text.value = "";
+});
